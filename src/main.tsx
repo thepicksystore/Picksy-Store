12f286a182505a214d3e178cfd1623389806edf0
Jay <jayjbhadreshwara@gmail.com>
2026-09-15 22:04:10 +0530

    Fix GitHub Pages router base path

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import './styles.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/Picksy-Store">
       <App />
     </BrowserRouter>
   </React.StrictMode>,
