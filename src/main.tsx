8c5d93575f800b4f03c3e0b0befd0fa2740e485a
Jay <jayjbhadreshwara@gmail.com>
2026-09-16 22:46:14 +0530

    Changes in main file

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import './styles.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter basename="/Picksy-Store">
+    <BrowserRouter basename={import.meta.env.BASE_URL}>
       <App />
     </BrowserRouter>
   </React.StrictMode>,
