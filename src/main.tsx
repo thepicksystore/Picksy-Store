18caa7b000e6669d4f8d489d65469944047c6de1
thepicksystore <thepicksystore@gmail.com>
2026-09-18 18:39:03 +0530

    Fix GitHub Pages router basename

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import './styles.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter basename={import.meta.env.BASE_URL}>
+    <BrowserRouter basename={import.meta.env.DEV ? '/' : '/Picksy-Store'}>
       <App />
     </BrowserRouter>
   </React.StrictMode>,
