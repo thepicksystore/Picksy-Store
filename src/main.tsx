05096f9dfe20af637b5866746f7fd410338f9dad
thepicksystore <thepicksystore@gmail.com>
2026-09-19 13:15:01 +0530

    Use Vite base URL for React Router basename

--- src/main.tsx
+++ src/main.tsx
@@ -4,9 +4,12 @@ import { BrowserRouter } from 'react-router-dom'
 import App from './App'
 import './styles.css'
 
+const basename =
+  import.meta.env.BASE_URL.replace(/\/$/, '') || '/'
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter basename={import.meta.env.DEV ? '/' : '/Picksy-Store'}>
+    <BrowserRouter basename={basename}>
       <App />
     </BrowserRouter>
   </React.StrictMode>,
